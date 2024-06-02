c7455c9d62124739fce890352ee65a7909189e01
Mohammad Rahat <mdrahat4595001@gmail.com>
2024-06-02 23:52:35 +0600

    added banner

--- src/pages/Dashboard/UserAnnounce/UserAnnounce.jsx
+++ src/pages/Dashboard/UserAnnounce/UserAnnounce.jsx
@@ -19,7 +19,7 @@ const UserAnnounce = () => {
                 All The Announcements are here.
             </h2>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-5">
                 {announcments.map((announcements) => (
                     <div key={announcements._id} className="card bg-white shadow-lg rounded-lg p-4">
                         <h3 className="text-2xl font-bold mb-2">{announcements.title}</h3>
