c081a68e00f8769f090e93054a7d56ce19e6ab29
Mohammad Rahat <mdrahat4595001@gmail.com>
2024-06-02 23:50:13 +0600

    showed announcement data in ui

--- src/pages/Dashboard/UserAnnounce/UserAnnounce.jsx
+++ src/pages/Dashboard/UserAnnounce/UserAnnounce.jsx
@@ -16,7 +16,7 @@ const UserAnnounce = () => {
     return (
         <div>
             <h2 className="text-4xl">
-                All The Announcements are here.: {announcments.length}
+                All The Announcements are here.
             </h2>
 
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
