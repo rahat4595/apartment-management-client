5f9abfe9db6f5cea7b73835e482aa53b783e9e26
Mohammad Rahat <mdrahat4595001@gmail.com>
2024-06-06 12:12:55 +0600

    showed the users and members separately

--- src/pages/Dashboard/AdminProfile/AdminProfile.jsx
+++ src/pages/Dashboard/AdminProfile/AdminProfile.jsx
@@ -1,62 +1,47 @@
 import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../../hooks/useAuth";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
-import { FaUser } from "react-icons/fa";
-
+import { FaUser, FaUsers } from "react-icons/fa";
 
 const AdminProfile = () => {
     const { user } = useAuth();
     const axiosSecure = useAxiosSecure();
 
-
-    const { data: stats } = useQuery({
+    const { data: stats = {} } = useQuery({
         queryKey: ['admin-stats'],
         queryFn: async () => {
             const res = await axiosSecure.get('/admin-stats');
-            return res.data
+            return res.data;
         }
     });
-
-
 
     return (
         <div>
             <h2 className="text-3xl">
                 <span>Hi, Welcome </span>
-                {
-                    user?.displayName ? user.displayName : 'Back'
-                }
+                {user?.displayName ? user.displayName : 'Back'}
             </h2>
 
-            <div className="stats shadow">
-
+            <div className="stats shadow mt-5">
                 <div className="stat">
                     <div className="stat-figure text-secondary">
-                        
-                    </div>
-                    <div className="stat-title">Downloads</div>
-                    <div className="stat-value">31K</div>
-                    <div className="stat-desc">Jan 1st - Feb 1st</div>
-                </div>
-
-                <div className="stat">
-                    <div className="stat-figure text-secondary">
-                       <FaUser></FaUser>
+                        <FaUser />
                     </div>
                     <div className="stat-title">Users</div>
                     <div className="stat-value">{stats.users}</div>
-                    <div className="stat-desc">↗︎ 400 (22%)</div>
+                    <div className="stat-desc">Total registered users</div>
                 </div>
 
                 <div className="stat">
                     <div className="stat-figure text-secondary">
-                        
+                        <FaUsers />
                     </div>
-                    <div className="stat-title">New Registers</div>
-                    <div className="stat-value">1,200</div>
-                    <div className="stat-desc">↘︎ 90 (14%)</div>
+                    <div className="stat-title">Members</div>
+                    <div className="stat-value">{stats.members}</div>
+                    <div className="stat-desc">Total registered members</div>
                 </div>
 
+                
             </div>
         </div>
     );
